d340d3ca0c5edc32ff6d527d2f50da59a4f3d73e
Sylvain Reynaud <sylvainreynaud@protonmail.com>
2020-02-07 17:26:48 +0100

    WlmApi.__auth() done

--- example.py
+++ example.py
@@ -1,6 +1,11 @@
+from wlmapi import WlmApi
+
 from configparser import ConfigParser
 config = ConfigParser()
 config.read('config.ini')
 
 # read values from a section
 API_KEY = config.get('Settings', 'API_KEY')
+HOST = config.get('Settings', 'HOST')
+
+api = WlmApi(HOST, API_KEY)
--- wlmapi.py
+++ wlmapi.py
@@ -7,24 +7,41 @@ def get_md5(s):
 
 
 class WlmApi:
-    def __init__(self, host, api_key, return_format):
+    def __init__(self, host, api_key):
         self.host = host if host[-1] == '/' else host + '/'
         self.api_key = api_key
-        self.return_format = return_format  # php, xml or json
+        self.return_format = "json"  # only json handled atm
         self.session = rq.Session()
         self.session.headers.update({'User-Agent': 'WLMAPIClass'})  # dunno if it's useless but I did it
+        self.authenticated = False
+        self.__auth()
 
-    def get_url(self, ressource_name=''):
+    def __get_url(self, ressource_name=''):
         return self.host + "wlmapi/2.0/" + self.return_format + "/" + ressource_name
 
-    def _auth(self):
-        # TODO : gere le fail
-        r_json = self.session.get(self.get_url('auth')).json()
-        token = get_md5(r_json["lock"] + self.api_key)
-        data = {
-            "key": token,
-            "support_emulation": 1,
-        }
-        r = self.session.post(self.get_url('auth'), data=data)
-
-        return  # TODO : ok ou pas
+    def __auth(self):
+        """
+        Authentificate with api key
+        :return: boolean, true if everything is ok, false then
+        """
+        try:
+            r_json = self.session.get(self.__get_url('auth')).json()
+            if r_json.get("success") == 1:
+                token = get_md5(r_json["lock"] + self.api_key)
+                data = {
+                    "key": token,
+                    "support_emulation": 1,
+                }
+            else:
+                print(r_json.get("ERROR"))
+                return False
+            r_json = self.session.post(self.__get_url('auth'), data=data).json()
+            if r_json.get("success") == 1:
+                self.authenticated = True
+                return True
+            else:
+                print(r_json.get("ERROR"))
+                return False
+        except Exception as e:
+            print(e)
+            return False
